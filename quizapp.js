030c403ef2fe103b5d6aeacb6a90bc349ebe58b1
Goddessri <130735969+Goddessri@users.noreply.github.com>
2023-04-24 23:28:05 +0800

    Update quizapp.js

--- quizapp.js
+++ quizapp.js
@@ -100,7 +100,7 @@ const questions = [
     question: 'Who is the best YouTuber?',
     answers: [
       { text: 'Web Dev Simplified', correct: true },
-      { text: 'Traversy Media', correct: true },
+      { text: 'Riane Billeras Channel', correct: true },
       { text: 'Dev Ed', correct: true },
       { text: 'Fun Fun Function', correct: true }
     ]
